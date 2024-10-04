7d48b9a3b347b7394e47c1342bee7c9347a3e439
Precious <92827167+Ariesdiamond12@users.noreply.github.com>
2024-10-04 22:56:34 +0200

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,25 +4,27 @@ import { Link } from "react-router-dom";
 
 function Login({ users }) {
   const navigate = useNavigate();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const [error, setError] = useState("");
 
   const log_user = () => {
     const storedUser = users;
+    let foundUser = false;
 
     for (let i = 0; i < storedUser.length; i++) {
       if (
         email === storedUser[i].email &&
         password === storedUser[i].password
       ) {
+        foundUser = true;
         navigate("/Todo");
         break;
-      } else {
-        setError("Email and Password is incorrect!!");
       }
+    }
+
+    if (!foundUser) {
+      setError("Email and Password are incorrect!!");
     }
   };
 
@@ -30,9 +32,10 @@ function Login({ users }) {
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       <div>
         <div className="max-w-[400px] w-full mx-auto">
-          {" "}
-          {/* Form */}
           <h2 className="text-4xl font-bold text-center py-6">Login</h2>
+
+          {error && <p className="text-red-500">{error}</p>}
+
           <div className="flex flex-col py-2">
             <label>Email</label>
             <input
@@ -41,6 +44,7 @@ function Login({ users }) {
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
+
           <div className="flex flex-col py-2">
             <label>Password</label>
             <input
@@ -49,6 +53,7 @@ function Login({ users }) {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+
           <button
             className="border-none rounded-full bg-rose-600 h-16 w-full pl-6 pr-2 text-white text-lg font-medium cursor-pointer"
             type="submit"
@@ -56,10 +61,13 @@ function Login({ users }) {
           >
             Sign In
           </button>
+
           <div>
             <p>
-              Create an account?
-              <Link to="/Registration">Registraion</Link>
+              Create an account?{" "}
+              <Link to="/Registration" className="text-blue-600">
+                Registration
+              </Link>
             </p>
           </div>
         </div>
